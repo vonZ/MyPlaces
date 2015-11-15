0b6f94ca173989dd4c61f6fe2b4e9e48747e2d67
vonZ <vv.zeipel@gmail.com>
2015-11-15 14:05:10 +0100

    Filter places by search

--- assets/dist/app.nomin.js
+++ assets/dist/app.nomin.js
@@ -33,7 +33,6 @@ app.controller("postCtrl", [ "$scope", "posts", "Map", "$timeout", "$location", 
             a.apiStatus = b;
         });
     };
-    a.states = [ "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" ];
     a.initOnSwitch = function() {};
     a.loadMap = function() {
         a.overviewMap();
